1191d3c195ddd8ea5728fa8812308bf7a92d899c
Bordone <bord78@gmail.com>
2023-10-17 13:21:47 -0400

    fixed remix configuration to ignore css files

--- app/routes/_header._controller._helper._index.tsx
+++ app/routes/_header._controller._helper._index.tsx
@@ -1,25 +1,25 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-import styles from './index.css'
+import type { MetaFunction } from '@remix-run/cloudflare';
+import styles from './index.css';
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
-export function links(){
-  return [{rel:'stylesheet', href: styles}]
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
 }
 
 export default function Index() {
   return (
     <>
-    <div className="content">
-      <h1>Hello</h1>
-      <h2>Remix</h2>
-      <button>Navigate</button>
-      <h3>Some description</h3>
-    </div>
+      <div className='content'>
+        <h1>Hello</h1>
+        <h2>Remix</h2>
+        <button>Navigate</button>
+        <h3>Some description</h3>
+      </div>
     </>
   );
 }
