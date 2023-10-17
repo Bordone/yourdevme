fa663addfc34e450d96c1ebda68faa9fc08bfe78
Bordone <bord78@gmail.com>
2023-10-17 12:54:39 -0400

    fixed an error not caught by the linter

--- app/routes/_header._controller/route.tsx
+++ app/routes/_header._controller/route.tsx
@@ -1,22 +1,21 @@
-import { Link, Outlet} from '@remix-run/react';
-import styles from './helper.css'
+import { Link, Outlet } from '@remix-run/react';
+import styles from './controller.css';
 
-export function links(){
-    return [{rel:'stylesheet', href: styles}]
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
 }
-
 
 export default function Controller() {
   return (
     <>
-    <div className='controller'>
-      <Link to='/quiz/blog'>Blog</Link>
-      <Link to='/quiz/about'>About Us</Link>
-      <Link to='/quiz/rebates'>Find Rebates</Link>
-      <Link to='/quiz/savings'>Discover Savings</Link>
-      <Link to='/quiz/stuff'>Other Stuff</Link>
-    </div>
-    <Outlet/>
+      <div className='controller'>
+        <Link to='/quiz/blog'>Blog</Link>
+        <Link to='/quiz/about'>About Us</Link>
+        <Link to='/quiz/rebates'>Find Rebates</Link>
+        <Link to='/quiz/savings'>Discover Savings</Link>
+        <Link to='/quiz/stuff'>Other Stuff</Link>
+      </div>
+      <Outlet />
     </>
   );
 }
